76b30e0940dcbac67242c0625847ddcbd5d8fb7b
mshivmca-lab <mshivmca@gmail.com>
2025-11-19 12:59:19 +0530

    Fix: Update Email.js file

--- server/config/email.js
+++ server/config/email.js
@@ -1,18 +1,3 @@
-// import dotenv from 'dotenv';
-// import nodemailer from 'nodemailer';
-
-// dotenv.config();
-
-// const transporter = nodemailer.createTransport({
-//   service: process.env.EMAIL_SERVICE||"gmail",
-//   auth: {
-//     user: process.env.EMAIL_USER||"mshivmca@gmail.com",
-//     pass: process.env.EMAIL_PASS||"dhvyxcnffiygoyat",
-//   },
-// });
-
-// export default transporter;
-
 
 import nodemailer from 'nodemailer';
 
